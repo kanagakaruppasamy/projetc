a8137ad679e5450a0d235617973fe18f27ef7efe
kanagakaruppasamy <kanaga.karuppasamy@gmail.com>
2022-08-22 10:44:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+setTimeout(function()
+{
+    img_id = "selfie1";
+    take_snapshot();
+    speak_data = "Taking Your Next Selfie In 10 Seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+},5000)
+
+function take_snapshot()
+{
+    console.log(img_id);
+}
+    Webcam.snap (function()
+    {
+        if(img_id=="selfie1")
+        {
+            document.getElementById("result1").innerHTML = '<img id="selfie1" src="'+data_uri+'"/>'   
+        }
+
+        if(img_id=="selfie2")
+        {
+            document.getElementById("result2").innerHTML = '<img id="selfie2" src="'+data_uri+'"/>'   
+        }
+
+        if(img_id=="selfie3")
+        {
+            document.getElementById("result3").innerHTML = '<img id="selfie3" src="'+data_uri+'"/>'   
+        }
+    });
